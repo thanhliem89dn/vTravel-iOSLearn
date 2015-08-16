ed7d1a6635a37da89696340b257cd0f72081d886
Lê Kim Tuấn <kimtuan257@gmail.com>
2015-08-16 15:26:05 +0700

    code them hanh trinh va them dia diem

--- vTravel/AddNewImageCell.swift
+++ vTravel/AddNewImageCell.swift
@@ -1,19 +1,15 @@
 //
-//  AddNewTripButtonCell.swift
+//  AddNewImageCell.swift
 //  vTravel
 //
-//  Created by Lê Kim Tuấn on 8/2/15.
+//  Created by Lê Kim Tuấn on 8/11/15.
 //  Copyright (c) 2015 iviettech. All rights reserved.
 //
 
 import UIKit
 
-class AddNewTripButtonCell: UITableViewCell {
+class AddNewImageCell: UITableViewCell {
 
-    @IBOutlet weak var btnAddPlace: UIButton!
-    @IBOutlet weak var btnSave: UIButton!
-    @IBOutlet weak var btnPost: UIButton!
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
